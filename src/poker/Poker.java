5dd8bb146bfffc57800684ad3a136a8a3ee7c484
Gumi <igumii26@gmail.com>
2019-05-23 13:52:31 -0500

    dsfasdfasdf

---
+++ src/poker/Poker.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package poker;
+
+
+public class Poker {
+
+    /**
+     * Poker.java -
+     *
+     * @author Mr. Wachs 
+     * @since 23-May-2019 
+     */
+    public static void main(String[] args) {
+     
+    PokerUI pokerui = new PokerUI(); //classes are capitsal
+    pokerui.setVisible(true);
+
+
+
+
+    }
+
+}
